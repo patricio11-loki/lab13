5e105d80f5bf76eb29bae62174e5beeb1561d7d1
patricio11-loki <61088358+patricio11-loki@users.noreply.github.com>
2024-11-14 16:38:02 -0500

    lab

--- script1.js
+++ script1.js
@@ -7,12 +7,10 @@ function suma(a, b) {
 function resta(a, b) {
     return a - b;
 }
-
 // Función de multiplicación
 function multiplicacion(a, b) {
     return a * b;
 }
-
 // Función de división
 function division(a, b) {
     if (b === 0) {
@@ -20,9 +18,27 @@ function division(a, b) {
     }
     return a / b;
 }
-// Ejemplos de uso
-document.write("Suma: ", suma(5, 3));           // Resultado: 8
-document.write("Resta: ", resta(5, 3));         // Resultado: 2
-document.write("Multiplicación: ", multiplicacion(5, 3)); // Resultado: 15
-document.write("División: ", division(5, 3));   // Resultado: 1.6666666666666667
-document.write("División por cero: ", division(5, 0)); // Resultado: "Error: División por cero"
+//función calculadora
+function calculadora(num1, num2, operacion) {
+    switch (operacion) {
+        case 'suma':
+            return suma (num1 + num2);
+        case 'resta':
+            return resta (num1 - num2);
+        case 'multiplicacion':
+            return multiplicacion (num1 * num2);
+        case 'division':
+            if (num2 !== 0) {
+                return division (num1 / num2);
+            } else {
+                return 'Error: División por cero';
+            }
+        default:
+            return 'Operación no válida';
+    }
+}
+let numero1=prompt("Ingrese el primer número:  ");
+let numero2=prompt("Ingrese el segundo número:  ");
+let operacion=prompt("Ingrese la operación (sumar, restar, multi, divi)");
+
+let resultado=calculadora(numero1, numero2, operacion);
